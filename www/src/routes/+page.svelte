1e5bca4361677d9e898e353a45bd245cf9fddd98
Tristan F <LeoDog896@hotmail.com>
2023-01-17 18:36:12 -0500

    docs(www): convert to vite

---
+++ www/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import "../index.css"
+    const images = import.meta.glob('./../../../ferret_images/collection/**/image.jpg')
+    const metadata = import.meta.glob('./../../../ferret_images/collection/**/image.json')
+    
+</script>
+
+<ul>
+    {#each Object.values(images) as image}
+        {@const img = image()}
+        {#await img then awaitedImg}
+            <li><img src={awaitedImg.default} /></li>
+        {/await}
+    {/each}
+    <li></li>
+</ul>
