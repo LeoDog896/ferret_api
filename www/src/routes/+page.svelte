450a84e39c833a0537dc8e928a326ab5e5ec828a
Tristan F <LeoDog896@hotmail.com>
2023-01-31 12:58:31 -0500

    fix(www): business href

--- www/src/routes/+page.svelte
+++ www/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <h1>Ferret API</h1>
 <p>A RESTful API for grabbing fast-loading images of ferrets.</p>
 
-<p>(Want a business of ferrets? Check out the <a href="ferret_api/business">business dump</a>!)</p>
+<p>(Want a business of ferrets? Check out the <a href="business">business dump</a>!)</p>
 
 <p>Also, I may or may not have gotten to the actual API part there. If you still want to wait, remind me or check this out on <a href="https://github.com/LeoDog896/ferret_api">github!</a></p>
